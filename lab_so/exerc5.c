d970763f030a5f7712bf50c20bcb1e2fa175906c
Luiz Irber <luiz.irber@gmail.com>
2007-05-11 14:58:23 +0000

    comeco do exercicio 5 de lab_so

---
+++ lab_so/exerc5.c
@@ -0,0 +1,17 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void * produtor(void* thread_num) {
+    pthread_t id;
+}
+
+void * consumidor(void* thread_num) {
+
+}
+
+int main (int argc, char** argv) {
+
+    return 0;
+
+}
